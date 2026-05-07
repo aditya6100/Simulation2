207129cf8a70e6460175e974a0f489bb104c6b24
Aditya Patil <adityabhanudas610@.com>
2026-05-07 12:26:48 +0530

    Move lab charts off window walls

--- script.js
+++ script.js
@@ -797,17 +797,19 @@ function addLabChartsForFloor(floorKey, elev) {
   const charts = topics[floorKey];
   if (!charts) return;
 
-  const leftZ = [21.4, 12.8, 3.5, -9.8];
-  const rightZ = [8.7, 0.2, -5.3];
+  const leftZ = [21.2, 12.4, 3.4, -9.6];
+  const rightZ = [8.5, 0.0, -5.0];
+  const leftRoomWallX = 46.70;
+  const rightRoomWallX = 49.56;
 
   leftZ.forEach((z, idx) => {
     const data = charts[idx % charts.length];
-    addWallChart(data[0], data[1], new THREE.Vector3(39.98, elev + 1.72, z), Math.PI / 2, { accent: data[2] });
+    addWallChart(data[0], data[1], new THREE.Vector3(leftRoomWallX, elev + 1.72, z), -Math.PI / 2, { accent: data[2] });
   });
 
   rightZ.forEach((z, idx) => {
     const data = charts[(idx + 1) % charts.length];
-    addWallChart(data[0], data[1], new THREE.Vector3(56.78, elev + 1.72, z), -Math.PI / 2, { accent: data[2] });
+    addWallChart(data[0], data[1], new THREE.Vector3(rightRoomWallX, elev + 1.72, z), Math.PI / 2, { accent: data[2] });
   });
 }
 
